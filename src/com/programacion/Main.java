5b08ca0bd7f180c43ef1bdb65d702055f7388750
AlexFernandezGonzalez <afernandezgonzalez@danielcastelao.org>
2020-10-30 08:51:32 +0100

    Coooomit

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -3,7 +3,7 @@ package com.programacion;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Principal");
-        System.out.println("Paquete principal");
+	System.out.println("Terminado ej 3");
+
     }
 }
